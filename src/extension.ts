01c9c1ff13051ea451cf4f6548e31c6fe3a1b0f2
Teagan Glenn <that@teagantotally.rocks>
2019-05-16 11:03:28 -0600

    Remove Telemetry

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,8 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
 import { LanguageClient, LanguageClientOptions, TransportKind, ServerOptions } from 'vscode-languageclient';
-import TelemetryReporter from 'vscode-extension-telemetry';
 
 const myExtensionId = 'vscode-home-assistant';
-const telemetryVersion = generateVersionString(vscode.extensions.getExtension(`keesschollaart.${myExtensionId}`));
-
-let reporter: TelemetryReporter;
 
 const documentSelector = [
     { language: 'home-assistant', scheme: 'file' },
@@ -16,14 +12,6 @@ const documentSelector = [
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('Home Assistant Extension has been activated!');
-
-    reporter = new TelemetryReporter(myExtensionId, telemetryVersion, 'ff172110-5bb2-4041-9f31-e157f1efda56');
-    try {
-        reporter.sendTelemetryEvent('extension.activate');
-    } catch (e) {
-        // if something bad happens reporting telemetry, swallow it and move on
-        console.log(`${e}`);
-    }
 
     var serverModule = path.join(context.extensionPath, 'out', 'server', 'server.js');
 
@@ -47,7 +35,6 @@ export function activate(context: vscode.ExtensionContext) {
     // is this really needed?
     vscode.languages.setLanguageConfiguration('home-assistant', { wordPattern: /("(?:[^\\\"]*(?:\\.)?)*"?)|[^\s{}\[\],:]+/ });
 
-    context.subscriptions.push(reporter);
     context.subscriptions.push(client.start());
 
     client.onReady().then(async () => {
@@ -60,7 +47,6 @@ export function activate(context: vscode.ExtensionContext) {
         });
     }).catch((reason) => {
         console.error(JSON.stringify(reason));
-        reporter.sendTelemetryEvent('extension.languageserver.onReadyError', { 'reason': JSON.stringify(reason) });
     });
 
     var fileAssociations = vscode.workspace.getConfiguration().get("files.associations");
@@ -70,7 +56,6 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function deactivate() {
-    reporter.dispose();
 }
 
 function generateVersionString(extension: vscode.Extension<any>): string {
